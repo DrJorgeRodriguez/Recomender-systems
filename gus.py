2f37ea7b396326515cd781e079a68852fbc50f7a
DrJorgeRodriguez <151852977+DrJorgeRodriguez@users.noreply.github.com>
2024-02-22 15:03:20 +0100

    added .py file

---
+++ gus.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+
+st.markdown("""
+    <h1 style='text-align: left; color: #009688;'>Welcome to WBS CUEVANA</h1>
+    """, unsafe_allow_html=True)
+
+st.markdown("""
+    <img src="https://media.giphy.com/media/l0HluKLonblTf8ili/giphy.gif?cid=790b7611im299mxs3q7bvnaoxeyjfsrk2q9sd54bxcqiz1a7&ep=v1_gifs_search&rid=giphy.gif&ct=g" width="300">
+    """, unsafe_allow_html=True)
